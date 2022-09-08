9a3cad0af3d4f65289aa7305e64dba5b549b38b5
Darren Bromwich <darrenb@kainos.com>
2022-09-08 16:33:37 +0100

    added trainer vm

--- components/idp/vm.tf
+++ components/idp/vm.tf
@@ -41,3 +41,34 @@ resource "azurerm_linux_virtual_machine" "application" {
   }
   tags = module.ctags.common_tags
 }
+
+resource "azurerm_linux_virtual_machine" "trainer" {
+  name                            = "idp-poc-trainer"
+  resource_group_name             = module.vnet.resource_group_name
+  location                        = var.location
+  size                            = "D16ads_v5"
+  admin_username                  = "adminuser"
+  disable_password_authentication = true
+  network_interface_ids = [
+    azurerm_network_interface.app_nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    #public_key = # key needed here
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 100
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20.04-LTS"
+    version   = "latest"
+  }
+  tags = module.ctags.common_tags
+}
