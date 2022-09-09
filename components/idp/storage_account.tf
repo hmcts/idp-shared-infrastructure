f57da4c73d54059fe693e80fc31afd82986b7a3f
Darren Bromwich <darrenb@kainos.com>
2022-09-09 13:54:38 +0100

    updated rg name instorage account

--- components/idp/storage_account.tf
+++ components/idp/storage_account.tf
@@ -2,7 +2,7 @@ module "storage_account_azcopy" {
   source                    = "git::https://github.com/hmcts/cnp-module-storage-account?ref=master"
   env                       = var.env
   storage_account_name      = "idp-poc-azcopy"
-  resource_group_name       = module.vnet.resource_group_name
+  resource_group_name       = module.vnet_name.resource_group_name
   location                  = var.location
   account_kind              = "StorageV2"
   account_tier              = "Standard"
@@ -17,7 +17,7 @@ module "storage_account_hyperscience" {
   source                    = "git::https://github.com/hmcts/cnp-module-storage-account?ref=master"
   env                       = var.env
   storage_account_name      = "idp-poc-hyperscience"
-  resource_group_name       = module.vnet.resource_group_name
+  resource_group_name       = module.vnet_name.resource_group_name
   location                  = var.location
   account_kind              = "StorageV2"
   account_tier              = "Standard"
