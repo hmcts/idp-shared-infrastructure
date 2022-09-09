8a14265111fce5019c631791a97155fb07355747
Darren Bromwich <darrenb@kainos.com>
2022-09-09 20:53:55 +0100

    added missing argument

--- components/idp/vnet.tf
+++ components/idp/vnet.tf
@@ -1,5 +1,5 @@
 module "vnet" {
-  source                        = "git::https://github.com/hmcts/cnp-module-vnet?ref=DTSPO-9934-update-address-prefixes"
+  source                        = "git@github.com:hmcts/cnp-module-vnet?ref=fix-address-prefix"
   name                          = var.vnet_name
   location                      = var.location
   address_space                 = var.address_space
@@ -7,5 +7,7 @@ module "vnet" {
   env                           = var.env
   lb_private_ip_address         = cidrhost(cidrsubnet(var.address_space, 4, 2), -2)
   postgresql_subnet_cidr_blocks = var.postgresql_subnet_cidr_blocks
+  postgresql_subnet_cidr        = var.postgresql_subnet_cidr
+
   common_tags                   = module.ctags.common_tags
 }
