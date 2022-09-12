922527d27da91ffdc3a86ce37c4facaa113faa51
mokainos <72137340+mokainos@users.noreply.github.com>
2022-09-12 10:48:32 +0100

    fmt

--- components/idp/storage_account.tf
+++ components/idp/storage_account.tf
@@ -1,36 +1,36 @@
- module "storage_account_azcopy" {
-   source                    = "git::https://github.com/hmcts/cnp-module-storage-account?ref=master"
-   env                       = var.env
-   storage_account_name      = "idp-poc-azcopy"
-   resource_group_name       = module.vnet.resource_group_name
-   location                  = var.location
-   account_kind              = "StorageV2"
-   account_tier              = "Standard"
-   account_replication_type  = "ZRS"
-   access_tier               = "Hot"
-   enable_https_traffic_only = true
-   sa_subnets                = module.vnet.subnet_ids
-   common_tags               = module.ctags.common_tags
- }
+module "storage_account_azcopy" {
+  source                    = "git::https://github.com/hmcts/cnp-module-storage-account?ref=master"
+  env                       = var.env
+  storage_account_name      = "idp-poc-azcopy"
+  resource_group_name       = module.vnet.resource_group_name
+  location                  = var.location
+  account_kind              = "StorageV2"
+  account_tier              = "Standard"
+  account_replication_type  = "ZRS"
+  access_tier               = "Hot"
+  enable_https_traffic_only = true
+  sa_subnets                = module.vnet.subnet_ids
+  common_tags               = module.ctags.common_tags
+}
 
- resource "azurerm_storage_share" "example" {
-   name                 = "azcopy"
-   storage_account_name = module.storage_account_azcopy.storageaccount_name
-   quota                = var.size_of_fileshare
+resource "azurerm_storage_share" "example" {
+  name                 = "azcopy"
+  storage_account_name = module.storage_account_azcopy.storageaccount_name
+  quota                = var.size_of_fileshare
 
- }
+}
 
- module "storage_account_hyperscience" {
-   source                    = "git::https://github.com/hmcts/cnp-module-storage-account?ref=master"
-   env                       = var.env
-   storage_account_name      = "idp-poc-hyperscience"
-   resource_group_name       = module.vnet.resource_group_name
-   location                  = var.location
-   account_kind              = "StorageV2"
-   account_tier              = "Standard"
-   account_replication_type  = "ZRS"
-   access_tier               = "Hot"
-   enable_https_traffic_only = true
-   sa_subnets                = module.vnet.subnet_ids
-   common_tags               = module.ctags.common_tags
- }
+module "storage_account_hyperscience" {
+  source                    = "git::https://github.com/hmcts/cnp-module-storage-account?ref=master"
+  env                       = var.env
+  storage_account_name      = "idp-poc-hyperscience"
+  resource_group_name       = module.vnet.resource_group_name
+  location                  = var.location
+  account_kind              = "StorageV2"
+  account_tier              = "Standard"
+  account_replication_type  = "ZRS"
+  access_tier               = "Hot"
+  enable_https_traffic_only = true
+  sa_subnets                = module.vnet.subnet_ids
+  common_tags               = module.ctags.common_tags
+}
