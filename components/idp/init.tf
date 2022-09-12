fac0c26d6ee9b2318dbad6e803139685165f19eb
mokainos <72137340+mokainos@users.noreply.github.com>
2022-09-12 11:19:21 +0100

    update

--- components/idp/init.tf
+++ components/idp/init.tf
@@ -11,19 +11,19 @@ terraform {
     }
   }
 }
-#
-#provider "azurerm" {
-#  subscription_id            = "ed302caf-ec27-4c64-a05e-85731c3ce90e"
-#  skip_provider_registration = "true"
-#  features {}
-#  alias = "vpn"
-#}
-#provider "azurerm" {
-#  subscription_id            = <>
-#  skip_provider_registration = "true"
-#  features {}
-#  alias = "hub-prod"
-#}
+
+provider "azurerm" {
+  subscription_id            = "ed302caf-ec27-4c64-a05e-85731c3ce90e"
+  skip_provider_registration = "true"
+  features {}
+  alias = "vpn"
+}
+provider "azurerm" {
+  subscription_id            = "0978315c-75fe-4ada-9d11-1eb5e0e0b214"
+  skip_provider_registration = "true"
+  features {}
+  alias = "hub-prod"
+}
 provider "azurerm" {
   features {}
 }
