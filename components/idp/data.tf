f53aaffe33f384b5014df246d029572f53aa4b43
Darren Bromwich <darrenb@kainos.com>
2022-09-13 20:27:59 +0100

    DOH - putting kv_secret_id in correct file

--- components/idp/data.tf
+++ components/idp/data.tf
@@ -3,6 +3,13 @@
 #  key_vault_id = data.azurerm_key_vault.kv.id
 #}
 
+data "azurerm_key_vault_secret" "kv_secret_id" {
+  name                = "wildcard-platform-hmcts.net"
+  resource_group_name = module.vnet.resourcegroup_name
+  location            = var.location
+  key_vault_secret_id = "https://idp-poc-prod.vault.azure.net/certificates/wildcard-platform-hmcts-net/efcaf31f43034f5a9ed74959e698b91e"
+}
+
 data "azurerm_client_config" "current" {}
 
 data "azurerm_key_vault_secret" "secret" {
