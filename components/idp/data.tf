b9dd503be0b6a3e3d127c454bf7b00aa9b6e6b0c
Darren Bromwich <darrenb@kainos.com>
2022-09-13 20:50:26 +0100

    added kv secret id

--- components/idp/data.tf
+++ components/idp/data.tf
@@ -4,9 +4,9 @@
 #}
 
 data "azurerm_key_vault_secret" "kv_secret_id" {
-  name         = "wildcard-platform-hmcts-net"
-  key_vault_id = module.idp-kv.key_vault_id
-  # key_vault_id = "https://idp-poc-prod.vault.azure.net/certificates/wildcard-platform-hmcts-net/efcaf31f43034f5a9ed74959e698b91e/"
+  name                = "wildcard-platform-hmcts-net"
+  key_vault_id        = module.idp-kv.key_vault_id
+  key_vault_secret_id = "https://idp-poc-prod.vault.azure.net/certificates/wildcard-platform-hmcts-net/efcaf31f43034f5a9ed74959e698b91e/"
 }
 
 data "azurerm_client_config" "current" {}
