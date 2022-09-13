13c2c726bb2eeb3b0065c3d4a7e36b078a3d8dfb
Darren Bromwich <darrenb@kainos.com>
2022-09-13 21:10:38 +0100

    update kv secret id

--- components/idp/appgw.tf
+++ components/idp/appgw.tf
@@ -53,8 +53,9 @@ resource "azurerm_application_gateway" "idp" {
   }
 
   ssl_certificate {
-    name                = "wildcard-platform-hmcts-net"
-    key_vault_secret_id = data.azurerm_key_vault_secret.kv_secret_id.key_vault_id.key_vault_secret_id
+    name = "wildcard-platform-hmcts-net"
+    # key_vault_secret_id = data.azurerm_key_vault_secret.kv_secret_id.key_vault_id.key_vault_secret_id
+    key_vault_secret_id = "https://idp-poc-prod.vault.azure.net/certificates/wildcard-platform-hmcts-net/efcaf31f43034f5a9ed74959e698b91e"
   }
 
   request_routing_rule {
