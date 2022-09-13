51d90818c0d7455f0a9d497f4e13552784224dc7
Darren Bromwich <darrenb@kainos.com>
2022-09-13 20:30:26 +0100

    removed location & rg from data block

--- components/idp/data.tf
+++ components/idp/data.tf
@@ -5,8 +5,6 @@
 
 data "azurerm_key_vault_secret" "kv_secret_id" {
   name                = "wildcard-platform-hmcts.net"
-  resource_group_name = module.vnet.resourcegroup_name
-  location            = var.location
   key_vault_secret_id = "https://idp-poc-prod.vault.azure.net/certificates/wildcard-platform-hmcts-net/efcaf31f43034f5a9ed74959e698b91e"
 }
 
