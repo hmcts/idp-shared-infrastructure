b499d5ca5936bdacda409c9d6884a8dfceec32ba
mokainos <72137340+mokainos@users.noreply.github.com>
2022-09-14 10:36:21 +0100

    update

--- components/idp/appgw.tf
+++ components/idp/appgw.tf
@@ -67,7 +67,7 @@ resource "azurerm_application_gateway" "idp" {
   }
   identity {
     type         = "UserAssigned"
-    identity_ids = azurerm_user_assigned_identity.id-agw.id
+    identity_ids = [azurerm_user_assigned_identity.id-agw.id]
   }
 }
 
