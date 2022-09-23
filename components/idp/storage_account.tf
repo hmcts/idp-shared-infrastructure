94ddca19b93fd8ea4c38ee283db1643b6c8bc4ff
mokainos <72137340+mokainos@users.noreply.github.com>
2022-09-23 15:52:29 +0100

    fmt

--- components/idp/storage_account.tf
+++ components/idp/storage_account.tf
@@ -39,20 +39,3 @@ module "storage_account_hyperscience" {
 
 }
 
-module "storage_account_hypersciencefile" {
-  source                    = "git::https://github.com/hmcts/cnp-module-storage-account?ref=master"
-  env                       = var.env
-  storage_account_name      = "idppohsciencefile"
-  resource_group_name       = module.vnet.resourcegroup_name
-  location                  = var.location
-  account_kind              = "StorageV2"
-  account_tier              = "Standard"
-  account_replication_type  = "ZRS"
-  access_tier               = "Hot"
-  enable_https_traffic_only = true
-  #sa_subnets               = module.vnet.subnet_ids
-  common_tags = module.ctags.common_tags
-  sa_subnets = [
-  "/subscriptions/a0939257-9c73-48ab-8daa-51cd49ef6c42/resourceGroups/idp-poc-infra-prod/providers/Microsoft.Network/virtualNetworks/idp-poc-infra-vnet-prod/subnets/iaas"]
-
-}
